817696ec255e79bd1c2c946422356476fd2816ec
Jonathan Chen <ypeels@live.com>
2012-11-20 19:25:28 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MhartlSampleApp::Application.config.secret_token = 'd80b4b2b79c83da33cdfae883b89d831b04a484f25558d79ac25538365d06ae1fe49373ea1d6427f9aa48fe1da0d42f5f8c860b001c150d2bffe9f81db7606b3'
