97c1f14d32c48b015b4459aee28203df4c78bdad
Jonathan Chen <ypeels@live.com>
2012-11-22 00:25:11 -0600

    Listings 3.17-3.18 html title tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,9 +4,16 @@ describe "Static pages" do
   
   # Listing 3.9
   describe "Home page" do
+  
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'Sample App')       
+    end
+    
+    # Listing 3.17 - goes here?
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
   
@@ -14,16 +21,33 @@ describe "Static pages" do
   describe "Help page" do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
-      page.should have_content('Help')
+      page.should have_selector('h1', :text => 'Help')
     end
+    
+    # Listing 3.18
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App | Help")
+    end
+    
   end
 
   # Listing 3.13
   describe "About page" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
-      page.should have_content('About Us')
+      page.should have_selector('h1', :text => 'About Us')
     end
+    
+    # Listing 3.18
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                    :text => "Ruby on Rails Tutorial Sample App | About Us")
+    end
+    
   end
+  
   
 end
