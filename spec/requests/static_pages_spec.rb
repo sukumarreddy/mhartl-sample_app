ce2f5122076e98e269a05d8e7775f5ad10e7a9df
Jonathan Chen <ypeels@live.com>
2012-11-22 00:14:56 -0600

    Listing 3.13 test for About

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -18,4 +18,12 @@ describe "Static pages" do
     end
   end
 
+  # Listing 3.13
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_content('About Us')
+    end
+  end
+  
 end
