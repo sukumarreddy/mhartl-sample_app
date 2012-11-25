f7908982f78f45191fba097483504fe4246bab87
Jonathan Chen <ypeels@live.com>
2012-11-25 10:25:42 -0600

    Listing 3.30 DRY out spec with let(:base_title) - but now spec is failing??

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
+  # Listing 3.30
+  let(:base_title) { "Ruby on Rails Sample App" }
   
   # Listing 3.9
   describe "Home page" do
@@ -13,7 +16,7 @@ describe "Static pages" do
     # Listing 3.17 - goes here?
     it "should have the right title 'Home'" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+      page.should have_selector('title', :text => "#{base_title} | Home")
     end
   end
   
@@ -28,7 +31,7 @@ describe "Static pages" do
     it "should have the title 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Help")
+                        :text => "#{base_title} | Help")
     end
     
   end
@@ -44,7 +47,7 @@ describe "Static pages" do
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('title',
-                    :text => "Ruby on Rails Tutorial Sample App | About Us")
+                    :text => "#{base_title} | About Us")
     end
     
   end
