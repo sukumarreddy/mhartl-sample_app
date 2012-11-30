4f12f2a619e335debc8c50b7c962e222825b7041
Jonathan Chen <ypeels@live.com>
2012-11-30 14:44:31 -0600

    Listing 6.16-6.17 TDD for email regex validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,9 +13,14 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name
 
   # Listing 6.9 - temporarily commented out in Listings 6.10-11 for "reverse TDD" or something
+  # Listing 6.15 - add length validation
   validates :name, presence: true, length: { maximum: 50 }
 
   # Listing 6.13
-  validates :email, presence: true
+  # Listing 6.17 add regex validation
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+
+
 
 end
