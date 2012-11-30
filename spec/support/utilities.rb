80db7cdebe7b1845ea65d3c8d6e338c75e5dbcff
Jonathan Chen <ypeels@live.com>
2012-11-29 23:44:25 -0600

    Listings 5.37-5.38 remove duplication of full_title() helper function in utilities.rb

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,9 +1,12 @@
 # Listing 5.26 (requires restarting Guard)
-def full_title(page_title)
-  base_title = "Ruby on Rails Tutorial Sample App"
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
-end
+#def full_title(page_title)
+#  base_title = "Ruby on Rails Tutorial Sample App"
+#  if page_title.empty?
+#    base_title
+#  else
+#    "#{base_title} | #{page_title}"
+#  end
+#end
+
+# Listing 5.38 - reaches back into app/helpers (searches by class name, I guess?)
+include ApplicationHelper
