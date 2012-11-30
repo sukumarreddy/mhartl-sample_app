54af760f59eb3143ebe8a59a2502bdb860519beb
Jonathan Chen <ypeels@live.com>
2012-11-30 17:33:13 -0600

    Listing 7.12-7.13 TDD for gravatar (3rd party API?)

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+
+  # Listing 7.13 - Returns the Gravatar (see gravatar.com) for the given user
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase) # downcase since MD5 is case-sensitive (emails are not)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
+
 end
