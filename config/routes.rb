64a5740ebca3835ee82e0d5706a22fdfd7f37d4e
Jonathan Chen <ypeels@live.com>
2012-11-29 22:36:14 -0600

    Listings 5.20-5.23: TDD for named routes, like about_path, and also delete public/index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,22 @@
 MhartlSampleApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about" # Listing 3.14
-  get "static_pages/contact" # Listing 5.17
+  #get "static_pages/home"
+  #get "static_pages/help"
+  #get "static_pages/about" # Listing 3.14
+  #get "static_pages/contact" # Listing 5.17
+
+  # Listing 5.21 - routes URI to StaticPagesController's methods
+  #              - gets most of Listing 5.20 to pass, but required restarting Guard
+  match '/help', to: 'static_pages#help'
+  match '/about', to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+
+  # Listing 5.22 - use this stock Rails comment as a hint.
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  # root :to => 'welcome#index'
+
+  # Listing 5.23 
+  root to: 'static_pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,10 +64,6 @@ MhartlSampleApp::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
