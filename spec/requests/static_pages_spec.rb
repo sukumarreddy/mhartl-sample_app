8d70ceb7a81f9df6f05488564d53552a09b870ae
Jonathan Chen <ypeels@live.com>
2012-11-29 22:58:57 -0600

    Listings 5.26-5.27 prettier RSpec tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,76 +5,99 @@ describe "Static pages" do
   # Listing 3.30
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   
+  # Listing 5.27
+  subject { page }
+
   # Listing 3.9
   describe "Home page" do
-  
-    it "should have the h1 'Sample App'" do
-      #visit '/static_pages/home'
-      visit root_path # Listing 5.20
-      page.should have_selector('h1', :text => 'Sample App')       
-    end
+
+    #it "should have the h1 'Sample App'" do
+    #  #visit '/static_pages/home'
+    #  #visit root_path # Listing 5.20
+    #  page.should have_selector('h1', :text => 'Sample App')       
+    #end
     
-    # Listing 3.17 - goes here?
-    it "should have the base title" do
-      #visit '/static_pages/home'
-      visit root_path
-      page.should have_selector('title', :text => "#{base_title}")
-    end
+    ## Listing 3.17 - goes here?
+    #it "should have the base title" do
+    #  #visit '/static_pages/home'
+    #  #visit root_path
+    #  page.should have_selector('title', :text => "#{base_title}")
+    #end
 
     # Listing 4.4 
-    it "should not have a custom page title" do
-      #visit '/static_pages/home'
-      visit root_path
-      page.should_not have_selector('title', :text => '| Home')
-    end
+    #it "should not have a custom page title" do
+    #  #visit '/static_pages/home'
+    #  #visit root_path
+    #  page.should_not have_selector('title', :text => '| Home')
+    #end
+    
+    # Listing 5.27
+    before { visit root_path }
+    it { should have_selector('h1', :text => 'Sample App') }
+    it { should have_selector('title', :text => full_title('')) }
+    it { should_not have_selector('title', :text => '| Home') }
 
   end
   
   # Listing 3.11
   describe "Help page" do
-    it "should have the content 'Help'" do
-      #visit '/static_pages/help'
-      visit help_path # Listing 5.20
-      page.should have_selector('h1', :text => 'Help')
-    end
+  
+    #it "should have the content 'Help'" do
+    #  #visit '/static_pages/help'
+    #  visit help_path # Listing 5.20
+    #  page.should have_selector('h1', :text => 'Help')
+    #end
+    #
+    ## Listing 3.18
+    #it "should have the title 'Help'" do
+    #  #visit '/static_pages/help'
+    #  visit help_path
+    #  page.should have_selector('title', :text => "#{base_title} | Help")
+    #end
     
-    # Listing 3.18
-    it "should have the title 'Help'" do
-      #visit '/static_pages/help'
-      visit help_path
-      page.should have_selector('title', :text => "#{base_title} | Help")
-    end
-    
+    # Listing 5.27
+    before { visit help_path }
+    it { should have_selector('h1', text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }    
   end
 
   # Listing 3.13
   describe "About page" do
-    it "should have the content 'About Us'" do
-      #visit '/static_pages/about'
-      visit about_path # Listing 5.20
-      page.should have_selector('h1', :text => 'About Us')
-    end
+    # it "should have the content 'About Us'" do
+    #   #visit '/static_pages/about'
+    #   visit about_path # Listing 5.20
+    #   page.should have_selector('h1', :text => 'About Us')
+    # end
+    # 
+    # # Listing 3.18
+    # it "should have the title 'About Us'" do
+    #   #visit '/static_pages/about'
+    #   visit about_path
+    #   page.should have_selector('title', :text => "#{base_title} | About Us")
+    # end
     
-    # Listing 3.18
-    it "should have the title 'About Us'" do
-      #visit '/static_pages/about'
-      visit about_path
-      page.should have_selector('title', :text => "#{base_title} | About Us")
-    end
-    
+    # Listing 5.27
+    before { visit about_path }
+    it { should have_selector('h1', text: 'About') }
+    it { should have_selector('title', text: full_title('About')) }  
   end
 
   # Listing 5.16
   describe "Contact page" do
-    it "should have the h1 'Contact'" do
-      visit contact_path
-      page.should have_selector('h1', text: 'Contact')
-    end
+    # it "should have the h1 'Contact'" do
+    #   visit contact_path
+    #   page.should have_selector('h1', text: 'Contact')
+    # end
+    # 
+    # it "should have the title 'Contact'" do
+    #   visit contact_path
+    #   page.should have_selector('title', text: "#{base_title} | Contact")
+    # end
 
-    it "should have the title 'Contact'" do
-      visit contact_path
-      page.should have_selector('title', text: "#{base_title} | Contact")
-    end
+    # Listing 5.27
+    before { visit contact_path }
+    it { should have_selector('h1', text: 'Contact') }
+    it { should have_selector('title', text: full_title('Contact')) } 
   end
   
   
---
+++ spec/support/utilities.rb
@@ -0,0 +1,9 @@
+# Listing 5.26 (requires restarting Guard)
+def full_title(page_title)
+  base_title = "Ruby on Rails Tutorial Sample App"
+  if page_title.empty?
+    base_title
+  else
+    "#{base_title} | #{page_title}"
+  end
+end
