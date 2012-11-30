48682b376cc1bba8402312d1214eb70185774ad1
Jonathan Chen <ypeels@live.com>
2012-11-30 13:20:26 -0600

    Listing 6.2 bundle exec rake db:migrate and some comments

--- db/migrate/20121130190958_create_users.rb
+++ db/migrate/20121130190958_create_users.rb
@@ -4,6 +4,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :name
       t.string :email
 
+      # Listing 6.2: auto-generated code to create magic columns created_at and updated_at
       t.timestamps
     end
   end
