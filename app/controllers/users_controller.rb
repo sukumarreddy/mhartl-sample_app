3345a77dfa6bce9791e30c05e663d7d8c955450c
Jonathan Chen <ypeels@live.com>
2012-11-30 18:16:29 -0600

    Listings 7.21-7.23 UsersController#create(), and display signup error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,15 @@ class UsersController < ApplicationController
   def show
   	@user = User.find params[:id]
   end
+
+  # Listing 7.21
+  def create
+  	@user = User.new params[:user]
+  	if @user.save
+  	  # Handle a successful save
+  	else
+  	  render 'new'
+  	end
+  end
+
 end
