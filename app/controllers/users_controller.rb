ddcb0c73e76d39bb01ecda540184fa86a7009bd2
Jonathan Chen <ypeels@live.com>
2012-11-30 18:00:38 -0600

    Listings 7.16-7.18 TDD for signup page - BASIC tests passing...

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new # Listing 7.18
   end
 
   # Listing 7.5
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -28,4 +28,32 @@ describe "UserPages" do
     it { should have_selector('h1', text: user.name) }
     it { should have_selector('title', text: user.name) }
   end
-end
+
+  # Listing 7.16
+  describe "signup" do
+
+    before { visit signup_path }
+
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do # signup information is blank unless specified otherwise
+        expect { click_button submit }.not_to change(User, :count) # invalid user should not get committed to db
+      end                                                          # {} syntax for change() invocation
+    end                                                            # User.count computed before and after {}
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User" # (supposedly) valid signup information
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end # signup
+
+end # UserPages
