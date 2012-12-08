d7b0562d127b244c3baa170ae4bf0868d0f614a4
Jonathan Chen <ypeels@live.com>
2012-12-07 23:05:24 -0600

    Listings 8.5-8.12 finished Section 8.1 - Sign-in sad case done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,25 @@ class SessionsController < ApplicationController
 
   # Listing 8.3 - empty new, create, destroy functions
   def new; end
-  def create; end
+
+  def create 
+
+    # Listing 8.10 failed signin
+    user = User.find_by_email params[:session][:email].downcase # because db saves as lowercase
+    if user && user.authenticate(params[:session][:password]) # uh, why does this need parens? meh
+      # Sign the user in and redirect to user's show page
+    else
+
+      # Listing 8.12 magical flash.now instead of flash
+      #flash[:error] = 'Invalid email/password combination' # "Not quite right"
+      flash.now[:error] = 'Invalid email/password combination'
+      
+      # Listing 8.9 preliminary
+      render 'new'  
+    end
+
+    
+  end
+  
   def destroy; end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -17,4 +17,37 @@ describe "AuthenticationPages" do
     it { should have_selector 'title', text: 'Sign in' }
   end
 
+  # Listing 8.5 - tests for signin failure
+  describe "signin action" do
+    before { visit signin_path }
+
+    describe "with invalid information" do
+      before { click_button "Sign in" }
+      it { should have_selector 'title', text: 'Sign in' } 
+      it { should have_selector 'div.alert.alert-error', text:'Invalid' }
+
+      # Listing 8.11 flash persists for 2 pages after 'render'
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector 'div.alert.alert-error' }
+      end
+    end
+
+    # Listing 8.6 - test for signin success
+    describe "with valid information" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        fill_in "Email", with: user.email
+        fill_in "Password", with: user.password
+        click_button "Sign in"
+      end
+
+      it { should have_selector 'title', text: user.name }
+      it { should have_link 'Profile', href: user_path(user) }
+      it { should have_link 'Sign out', href: signout_path }
+      it { should_not have_link 'Sign in', href: signin_path }
+    end
+
+  end
+
 end
