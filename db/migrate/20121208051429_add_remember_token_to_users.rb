0bd2e039032b98446f59e13fd7018db7b1842dbc
Jonathan Chen <ypeels@live.com>
2012-12-07 23:32:21 -0600

    Listing 8.16 + bundle exec rake db:migrate + bundle exec rake db:test:prepare

--- db/migrate/20121208051429_add_remember_token_to_users.rb
+++ db/migrate/20121208051429_add_remember_token_to_users.rb
@@ -1,4 +1,7 @@
 class AddRememberTokenToUsers < ActiveRecord::Migration
   def change
+    # Listing 8.16
+    add_column :users, :remember_token, :string
+    add_index :users, :remember_token
   end
 end
