b312b25d36b82133c1fbaa87ae76e40b2606f33c
Jonathan Chen <ypeels@live.com>
2012-12-09 15:14:45 -0600

    Listing 9.40. DON'T FORGET: bundle exec rake db:migrate bundle exec rake db:test:prepare

--- db/migrate/20121209211217_add_admin_to_users.rb
+++ db/migrate/20121209211217_add_admin_to_users.rb
@@ -1,5 +1,9 @@
 class AddAdminToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :admin, :boolean
+
+    # Listing 9.40 explicitly add default: false - users should NOT be administrators by default.    
+    # system default is actually "nil", but this shows intent more explicitly.
+    add_column :users, :admin, :boolean, default: false
+
   end
 end
