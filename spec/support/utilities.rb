1ccd48bc4c89f83bd75716e8e9a45440f7e8f746
Jonathan Chen <ypeels@live.com>
2012-12-08 18:49:44 -0600

    Listing 8.34 - done with Chapter 8 up to conclusion

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -10,3 +10,16 @@
 
 # Listing 5.38 - reaches back into app/helpers (searches by class name, I guess?)
 include ApplicationHelper
+
+# Listing 8.34
+def valid_signin(user)
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+end
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-error', text: message)
+  end
+end
