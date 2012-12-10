ece8cafa1f39f99cb50316198cda91b947de8759
Jonathan Chen <ypeels@live.com>
2012-12-09 19:55:56 -0600

    Listings 10.17-10.18 Microposts < 140 chars

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -10,6 +10,9 @@ class Micropost < ActiveRecord::Base
   # Listing 10.4
   validates :user_id, presence: true
 
+  # Listing 10.18
+  validates :content, presence: true, length: { maximum: 140 }
+
   # Listing 10.14
   default_scope order: 'microposts.created_at DESC'
 end
--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -30,6 +30,17 @@ describe Micropost do
     it { should_not be_valid }
   end
 
+  # Listing 10.7 - validations
+  describe "with blank content" do
+    before { @micropost.content = " " }
+    it { should_not be_valid }
+  end
+
+  describe "with content that is too long" do
+    before { @micropost.content = "a" * 141 } # remember string multiplication? (Section 6.2)
+    it { should_not be_valid }
+  end
+
   # Listing 10.5
   describe "accessible attributes" do
     it "should not allow access to user_id" do
