2c3034782997844c75b3e2015c7e2bfd2d5fc217
Jonathan Chen <ypeels@live.com>
2012-12-11 15:32:24 -0600

    That's all she wrote, and I hope I never have to touch another piece of Rails code again in my life... Exercise 11.2 (Listing 11.47) - minor refactoring in RelationshipsController

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,16 +2,21 @@
 class RelationshipsController < ApplicationController
   before_filter :signed_in_user # extra security (command-line create/destroy would hit nil current_user anyway)
 
+  # Listing 11.47 (Exercise 11.2)
+  respond_to :html, :js
+
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
 
-    # Listing 11.38
-    #redirect_to @user
-    respond_to do |format|
-      format.html { redirect_to @user }
-      format.js # Rails will automatically load the corresponding .js.erb file
-    end
+    # Listing 11.47 (Exercise 11.2) - see also respond_to(), inside class definition
+    ## Listing 11.38
+    ##redirect_to @user
+    #respond_to do |format|
+    #  format.html { redirect_to @user }
+    #  format.js # Rails will automatically load the corresponding .js.erb file
+    #end
+    respond_with @user
 
   end
 
@@ -19,12 +24,14 @@ class RelationshipsController < ApplicationController
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow!(@user)
     
-    # Listing 11.38
-    #redirect_to @user
-    respond_to do |format|
-      format.html { redirect_to @user }
-      format.js
-    end
+    # Listing 11.47 (Exercise 11.2)
+    ## Listing 11.38
+    ##redirect_to @user
+    #respond_to do |format|
+    #  format.html { redirect_to @user }
+    #  format.js
+    #end
+    respond_with @user
 
   end
 end
