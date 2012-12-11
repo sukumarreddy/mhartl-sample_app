2dc7b9fd6d59f649808607395dd916a02aac5789
Jonathan Chen <ypeels@live.com>
2012-12-11 15:09:18 -0600

    Listing 11.45 status feed optimized with SQL subselect

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -19,12 +19,17 @@ class Micropost < ActiveRecord::Base
   # Listing 11.43 "a first cut"
   # Returns microposts from the users being followed by the given user.
   def Micropost.from_users_followed_by(user)
-    followed_user_ids = user.followed_user_ids
     
-    # Listing 11.44 - preparing syntax for SQL "subselect" (so far NO change)
-    #where("user_id IN (?) OR user_id = ?", followed_user_ids, user)
-    where("user_id IN (:followed_user_ids) OR user_id = :user_id",
-          followed_user_ids: followed_user_ids, user_id: user)
+    # Listing 11.45 - "formidable combination of Rails, Ruby, and SQL"
+    #followed_user_ids = user.followed_user_ids
+    followed_user_ids = "SELECT followed_id FROM relationships
+                         WHERE follower_id = :user_id"    # SQL subselect, which is more efficient? (and lower-level...)
+    ## Listing 11.44 - preparing syntax for SQL "subselect" (so far NO change)
+    ##where("user_id IN (?) OR user_id = ?", followed_user_ids, user)
+    #where("user_id IN (:followed_user_ids) OR user_id = :user_id",
+    #      followed_user_ids: followed_user_ids, user_id: user)
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id", 
+          user_id: user.id) # n.b. raw SQL can be interpolated; need not be escaped. way to double-down on SQL...
   end
 
 end
