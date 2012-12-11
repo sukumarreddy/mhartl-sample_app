084ce72596fe8f78a98aa60077a41bc2344ba14c
Jonathan Chen <ypeels@live.com>
2012-12-10 20:01:11 -0600

    Section 11.1.1 done: Listing 11.1 + bundle exec rake db:migrate && bundle exec rake db:test:prepare

--- db/migrate/20121211015700_create_relationships.rb
+++ db/migrate/20121211015700_create_relationships.rb
@@ -6,5 +6,11 @@ class CreateRelationships < ActiveRecord::Migration
 
       t.timestamps
     end
+
+    # Listing 11.1
+    add_index :relationships, :follower_id
+    add_index :relationships, :followed_id
+    add_index :relationships, [:follower_id, :followed_id], unique: true # enforces uniqueness of pairs (can't follow more than once)
+                                                                        # alternative: validation
   end
 end
