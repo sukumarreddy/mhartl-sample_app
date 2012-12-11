9ff75465bc4b99d577922c7f6b0be5f8377f7c2d
Jonathan Chen <ypeels@live.com>
2012-12-11 13:20:01 -0600

    Listings 11.7-11.8 quick validations on followe*_id presence

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,5 @@
 class Relationship < ActiveRecord::Base
+  
   # Listing 11.6 - override Rails' auto-generated accessibility
   #attr_accessible :followed_id, :follower_id
   attr_accessible :followed_id
@@ -6,4 +7,9 @@ class Relationship < ActiveRecord::Base
   # Listing 11.6
   belongs_to :follower, class_name: "User" # override Rails' default of Follower
   belongs_to :followed, class_name: "User" # override Rails' default of Followed
+
+  # Listing 11.8
+  validates :follower_id, presence: true
+  validates :followed_id, presence: true
+
 end
